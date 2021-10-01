6685c3cf2a4f29d1e391351ac88c8e115159b266
Ursula Wilborn <wilbornu@yahoo.com>
2021-09-30 23:29:56 -0400

    upadate routes

--- models/exercise.js
+++ models/exercise.js
@@ -3,18 +3,22 @@ const Schema = mongoose.Schema;
 
 
 const exerciseSchema = new Schema ({
-  type: {type: Text, required: true},
-  cardio_name: {type: Text, required: true},
-  distance: {type: Number, required: true},
+  day: {
+    type:Date,
+    defaut:Date.now
+  },
+  exercises: [{
+  type: {type: String, required: true},
+  name: {type: String, required: true},
   duration: {type: Number, required: true},
-  name: {type: Text, required: true},
+  distance: {type: Number, required: true},
   weight: {type: Number, required: true},
   sets: {type: Number, required: true},
   reps: {type: Number, required: true},
-  resistance_duration:{type: Number, required: true}
+      }],
     });
- 
 
-const Exercise = mongoose.model('Excercise', exerciseSchema);
+
+ const Exercise = mongoose.model('Excercise', exerciseSchema);
 
 module.exports = Exercise;
---
+++ routers/schemaroutes.js
@@ -0,0 +1,16 @@
+var router = require("express").Router()
+var path = require("path")
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname,"index.html"))
+} );
+
+router.get('/stats', function(req, res) {
+    res.sendFile(path.join(__dirname,"stats.html"))
+} );
+
+router.get('/exercise', function(req, res) {
+    res.sendFile(path.join(__dirname,"exercise.html"))
+} );
+
+module.exports = router;
